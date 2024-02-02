a3221ab3b07cc2e4d26ba767e26bf386ce7c548c
Max <max-j@gmx.de>
2024-02-02 14:42:40 +0100

    Add AuctionsController to handle auction-related API endpoints

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,56 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuctionsController : ControllerBase
+    {
+        private readonly AuctionDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AuctionsController(AuctionDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }      
+
+    
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+        {
+            var auctions = await _context.Auctions
+                .Include(a => a.Item)
+                .OrderBy(a => a.EndedAt)
+                .ToListAsync();
+
+            var auctionDtos = _mapper.Map<List<AuctionDto>>(auctions);
+
+            return Ok(auctionDtos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+        {
+            var auction = await _context.Auctions
+                .Include(a => a.Item)
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (auction == null)
+            {
+                return NotFound();
+            }
+
+            var auctionDto = _mapper.Map<AuctionDto>(auction);
+
+            return Ok(auctionDto);
+        }
+    }
+
+}
