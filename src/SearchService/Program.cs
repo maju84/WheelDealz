59313220267c31cad14fcda1d70f949cb4c5450a
Max <max-j@gmx.de>
2024-02-09 11:25:52 +0100

    Add MongoDB integration and initialize database with indexes

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,5 +14,18 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// initialize the database
+await DB.InitAsync("SearchDb", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection"))
+);
+
+// create the indexes for Make, Model and Color to enable text search
+await DB.Index<Item>()
+    .Key(a => a.Make, KeyType.Text)
+    .Key(a => a.Model, KeyType.Text)
+    .Key(a => a.Color, KeyType.Text)
+    .CreateAsync(); // non-blocking to avoid slowing down the app startup
+
 
 app.Run();
