84305feaf6c5e7bb9b1f7bf317e43ea5cb99149e
Max <max-j@gmx.de>
2024-02-12 12:19:54 +0100

    Configure message retry for auction created endpoint.

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -4,6 +4,11 @@ using SearchService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Define constant for service name
+const string SearchServiceName = "search-svc";
+
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -13,10 +18,16 @@ builder.Services.AddMassTransit(x =>
 {
     x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
 
-    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search-svc", false));
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter(SearchServiceName, false));
 
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.ReceiveEndpoint($"{SearchServiceName}-auction-created", e => 
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));   // 5 times, 5s between each try
+
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
         cfg.ConfigureEndpoints(context);
     });
 });
