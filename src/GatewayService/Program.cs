0b85838ed2cb58ae318b669c882a3cdc8e84ac62
Max <max-j@gmx.de>
2024-02-14 10:35:46 +0100

    Configure ReverseProxy routes, partially with auth².

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add the reverse proxy capability to the server
@@ -5,9 +7,21 @@ builder.Services.AddReverseProxy()
     // Initialize the reverse proxy from the "ReverseProxy" section of configuration
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 // Register the reverse proxy routes
 app.MapReverseProxy();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
