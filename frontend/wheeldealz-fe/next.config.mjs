5e0278ec92fbacffbb1e5182c930776f5e0abb37
Max <max-j@gmx.de>
2024-02-15 14:44:18 +0100

    Add image domains to next.config.

--- frontend/wheeldealz-fe/next.config.mjs
+++ frontend/wheeldealz-fe/next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+      domains: [
+        'cdn.pixabay.com',
+      ]
+    },
     logging: {
         fetches: {
           fullUrl: true,
