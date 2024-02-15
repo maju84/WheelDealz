3f1e59d3dd2680ff054776351647407d0e6a8fcc
Max <max-j@gmx.de>
2024-02-15 11:58:00 +0100

    Add Listings component and fetch data in Home component.

--- frontend/wheeldealz-fe/app/page.tsx
+++ frontend/wheeldealz-fe/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import Listings from "./auctions/Listings";
+
+async function getData() {
+  const response = await fetch("http://localhost:6001/search");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return response.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <div>
-      Content goes here
+      <Listings />
+      {JSON.stringify(data, null, 2)}
     </div>
   );
 }
