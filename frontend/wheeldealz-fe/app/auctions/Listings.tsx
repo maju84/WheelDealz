0c55399cf38f7762954d20a9373ae972751a8b36
Max <max-j@gmx.de>
2024-02-16 11:14:46 +0100

    State uplift from AppPagination into Listings. Enables controlling pagination.

--- frontend/wheeldealz-fe/app/auctions/Listings.tsx
+++ frontend/wheeldealz-fe/app/auctions/Listings.tsx
@@ -1,31 +1,37 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import AuctionCard from './AuctionCard';
-import { Auction, PagedResult } from '@/types';
 import AppPagination from '../components/AppPagination';
+import { getData } from '../actions/GetAuctionsAction';
+import { Auction } from '@/types';
 
 
-async function getData() : Promise<PagedResult<Auction>> {
-  const response = await fetch("http://localhost:6001/search?pageSize=4");
+export default function Listings() { 
+  const [auctions, setAuctions] = useState<Auction[]>([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(1);
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch data");
+  useEffect(() => {
+    getData(page).then(data => {
+      setAuctions(data.results);
+      setPageCount(data.pageCount);
+    });
+  }, [page]); // re-run when page changes, causes rerender
+
+  if (auctions.length === 0) {
+    return <h3>Loading...</h3>;
   }
-
-  return response.json();
-}
-
-export default async function Listings() {
-  const data = await getData();
   
   return (
     <>  {/* React fragment used as the single root element */}
       <div className='grid grid-cols-4 gap-6'>
-        {data && data.results.map(auction => (
+        {auctions.map(auction => (
           <AuctionCard auction={ auction } key={ auction.id } />
           ))}
       </div>
       <div className='flex justify-center mt-4'>
-        <AppPagination currentPage={1} pageCount={data.pageCount} />
+        <AppPagination pageChanged={setPage} currentPage={page} pageCount={pageCount} />
       </div>
     </>
     
--- frontend/wheeldealz-fe/app/components/AppPagination.tsx
+++ frontend/wheeldealz-fe/app/components/AppPagination.tsx
@@ -1,19 +1,20 @@
 'use client';
 
 import { Pagination } from 'flowbite-react';
-import React, { useState } from 'react';
+import React from 'react';
 
 type Props = {
     currentPage: number;
     pageCount: number;
+    // Function to call when a new page is selected.
+    pageChanged: (page: number) => void;
 }
 
-export default function AppPagination({ currentPage, pageCount }: Props) {
-    const [page, setPage] = useState(currentPage);
+export default function AppPagination({ currentPage, pageCount, pageChanged }: Props) {
   return (
     <Pagination
-        currentPage={page}
-        onPageChange={(newPage) => setPage(newPage)}
+        currentPage={currentPage}
+        onPageChange={(newPage) => pageChanged(newPage)}
         totalPages={pageCount}
         layout='pagination'
         showIcons={true}
