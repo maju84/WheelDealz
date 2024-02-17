43f54cb48a9c910e049f2aeacbeb9256ee496f7d
Max <max-j@gmx.de>
2024-02-17 11:59:56 +0100

    Search functionality works.

--- frontend/wheeldealz-fe/app/nav/Search.tsx
+++ frontend/wheeldealz-fe/app/nav/Search.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+'use client';
+
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 import { FaSearch } from "react-icons/fa";
+import { useParamsStore } from '../hooks/useParamsStore';
 
 export default function Search() {
+    const setParams = useParamsStore((state) => state.setParams);   
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') search();
+    };
+
+    const search = () =>{
+        setParams({searchTerm: searchTerm});
+    };
+
   return (
     <div className='flex w-[50%] items-center border-2 rounded-full py-2 shadow-sm'>
         <input 
+            onKeyDown={handleKeyDown}
+            onChange={onChange}
             type="text" 
             placeholder="Search for cars by make, model or color" 
             className='
@@ -13,7 +33,7 @@ export default function Search() {
                 focus:ring-transparent border-transparent 
                 text-sm text-gray-600'
         />
-        <button>
+        <button type="button" onClick={search}>   {/* just pass in the search function  */}
           <FaSearch size={38}
             className='bg-green-400 text-white rounded-full p-2 cursor-pointer mx-2' />
         </button>
