f651c6396d42a583bf19f755f55df5ef2b11cafe
Max <max-j@gmx.de>
2024-02-17 10:24:59 +0100

    Refactor next.config.mjs to use remotePatterns for images and got rid of deprecated warning.

--- frontend/wheeldealz-fe/next.config.mjs
+++ frontend/wheeldealz-fe/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: [
-        'cdn.pixabay.com',
+      remotePatterns: [
+        { hostname: 'cdn.pixabay.com' },
       ]
     },
     logging: {
