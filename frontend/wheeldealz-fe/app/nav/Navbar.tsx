1361ea15f7ccb0bdf6d6a84bd28c6ff772ae61f1
Max <max-j@gmx.de>
2024-02-19 13:48:28 +0100

    Add DropDown menu to user actions.

--- frontend/wheeldealz-fe/app/nav/Navbar.tsx
+++ frontend/wheeldealz-fe/app/nav/Navbar.tsx
@@ -16,7 +16,7 @@ export default async function Navbar() {
       <Logo />
       <Search />
       { user ? (
-        <UserActions />
+        <UserActions user={ user }/>
       ) : (
         <LoginButton />
       )}      
