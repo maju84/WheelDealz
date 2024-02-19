4bb3f33a1577f9c11944aeae5056e79a74f769c5
Max <max-j@gmx.de>
2024-02-19 13:58:50 +0100

    Add navigation functionality to Logo component.

--- frontend/wheeldealz-fe/app/nav/Logo.tsx
+++ frontend/wheeldealz-fe/app/nav/Logo.tsx
@@ -4,11 +4,22 @@ import React from 'react';
 import { GiCarWheel } from 'react-icons/gi';
 import { RiAuctionLine } from 'react-icons/ri';
 import { useParamsStore } from '../hooks/useParamsStore';
+import { usePathname, useRouter } from 'next/navigation';
 
 export default function Logo() {
+    const router = useRouter();
+    const pathname = usePathname();
     const reset = useParamsStore(state => state.reset);
+
+    const resetAndNavigateHome = () => {
+        if (pathname !== '/') {
+          router.push('/');            
+        } 
+        reset();
+    };
+
   return (
-    <div onClick={reset}
+    <div onClick={resetAndNavigateHome}
         className='
             cursor-pointer flex items-center gap-2 text-3xl font-semibold text-green-500'>
         <GiCarWheel size={42}/>
