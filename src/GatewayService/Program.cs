2fa4b699f9dab2eba2d0d10b4a04133fefe54b6d
Max <max-j@gmx.de>
2024-02-24 13:39:20 +0100

    Add CORS support on the Gateway.

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -16,7 +16,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username";
     });
 
+// Add CORS policy to allow the client app to access the gateway service
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", b =>  // ! use exactly this name in appsettings.json !
+    {
+        b.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
+
+
 var app = builder.Build();
+
+app.UseCors();
 
 // Register the reverse proxy routes
 app.MapReverseProxy();
