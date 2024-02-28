589938db196a11fdbf2affc06603032e73e1adaf
Max <max-j@gmx.de>
2024-02-28 11:31:16 +0100

    Add AuctionCreatedToast component and update SignalRProvider to take a user.

---
+++ frontend/wheeldealz-fe/app/components/toasts/AuctionCreatedToast.tsx
@@ -0,0 +1,27 @@
+import { Auction } from '@/types';
+import Link from 'next/link';
+import Image from 'next/image';
+import React from 'react';
+
+type Props = {
+    auction: Auction
+}
+
+const AUCTION_DETAILS_ENDPOINT = '/auctions/details/';
+
+export default function AuctionCreatedToast({ auction }: Props) {
+  return (
+    <Link href={`${AUCTION_DETAILS_ENDPOINT}${auction.id}`} className='flex flex-col items-center'>
+        <div className='flex flex-row items-center gap-2'>
+            <Image
+                src={ auction.imageUrl }
+                alt='auction-image'
+                height={80}
+                width={80}
+                className='rounded-lg w-auto h-auto'
+            />        
+        <span>New Auction! {auction.make} {auction.model} has been added.</span>
+     </div>
+    </Link>
+  );
+}
--- frontend/wheeldealz-fe/app/providers/SignalRProvider.tsx
+++ frontend/wheeldealz-fe/app/providers/SignalRProvider.tsx
@@ -3,22 +3,34 @@
 import { ReactNode, useEffect, useState } from 'react';
 import { useAuctionsStore } from '../hooks/useAuctionsStore';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
-import { Bid } from '@/types';
+import { Auction, Bid } from '@/types';
 import { useBidsStore } from '../hooks/useBidsStore';
 import { usePathname } from 'next/navigation';
+import { Session } from 'next-auth';
+import AuctionCreatedToast from '../components/toasts/AuctionCreatedToast';
+import toast from 'react-hot-toast';
 
 const BID_PLACED_EVENT_NAME = 'BidPlaced';  // ! must match exactly sender's magic string !
+const AUCTION_CREATED_EVENT_NAME = 'AuctionCreated'; 
+
 const NOTIFICATIONS_URL = 'http://localhost:6001/notifications';
 
 
 type Props = {
     children: ReactNode
+    user: Session['user'] | null
 }
-export default function SignalRProvider({ children }: Props) {
+
+export default function SignalRProvider({ children, user }: Props) {
     const [connection, setConnection] = useState<HubConnection | null>(null);
     const { setCurrentPrice } = useAuctionsStore();
     const { addBid } = useBidsStore();
     const pathname = usePathname();
+
+    const showAuctionCreatedToast = (auction: Auction) => {
+        toast(() => <AuctionCreatedToast auction={ auction } />
+        ), { duration: 10_000 };
+    };
 
     useEffect(() => {
         const newConnection = new HubConnectionBuilder()
@@ -33,26 +45,36 @@ export default function SignalRProvider({ children }: Props) {
         if (connection) {
             connection.start()
                 .then( () => {
-                    connection.on(BID_PLACED_EVENT_NAME,  
-                        (bid: Bid) => {
-                            if (bid.bidStatus.includes('Accepted')) {
-                                setCurrentPrice(bid.auctionId, bid.amount);
-                            }
-                            // only add bids if we are on this auction's details page
-                            if(pathname.includes(bid.auctionId)) {
-                                addBid(bid);
-                              }
+
+                    connection.on(BID_PLACED_EVENT_NAME, (bid: Bid) => {
+                        if (bid.bidStatus.includes('Accepted')) {
+                            setCurrentPrice(bid.auctionId, bid.amount);
+                        }
+                        // only add bids if we are on this auction's details page
+                        if(pathname.includes(bid.auctionId)) {
+                            addBid(bid);
+                        }
                     });
+
+                    connection.on(AUCTION_CREATED_EVENT_NAME, (auction: Auction) => {
+                        if (user?.username !== auction.seller) {
+                            showAuctionCreatedToast(auction);
+                        }
+                    });
+
                 }).catch(err => console.log(err));
+
+
         }
 
         return () => {
             connection?.stop();
             connection?.off(BID_PLACED_EVENT_NAME);
+            connection?.off(AUCTION_CREATED_EVENT_NAME);
 
         };
 
-    }, [addBid, pathname, connection, setCurrentPrice]);
+    }, [addBid, pathname, connection, setCurrentPrice, user]);
 
   return (
     children
