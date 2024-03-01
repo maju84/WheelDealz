9da9dfd246a15d51e30552466529324c3a0bb2b1
Max <max-j@gmx.de>
2024-03-01 15:55:08 +0100

    Identity Svc extract hardcoded RedirectUrl into config.

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -18,7 +18,7 @@ public static class Config
             new ApiScope("auction-app", "Auction ap full access"),
         };
 
-    public static IEnumerable<Client> Clients =>
+    public static IEnumerable<Client> Clients(IConfiguration config) =>
         new Client[]
         {
             // a client config for development purposes only!
@@ -40,7 +40,7 @@ public static class Config
                 ClientSecrets = new [] {new Secret("secret".Sha256())},                              
                 AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
                 RequirePkce = false,
-                RedirectUris = { "http://localhost:3000/api/auth/callback/id-server" },   
+                RedirectUris = { config["ClientApp"] + "/api/auth/callback/id-server" },   
                 AllowOfflineAccess = true,
                 AllowedScopes = { "openid", "profile", "auction-app" },
                 AccessTokenLifetime = 3600*24*30,
