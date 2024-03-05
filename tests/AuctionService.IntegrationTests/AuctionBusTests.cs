4ed37e2ad29dd1352d1f7361294d886c06564a95
Max <max-j@gmx.de>
2024-03-05 10:28:53 +0100

    Add AuctionBusTests for integration testing.

---
+++ tests/AuctionService.IntegrationTests/AuctionBusTests.cs
@@ -0,0 +1,54 @@
+using System.Net;
+using System.Net.Http.Json;
+using AuctionService.Data;
+using AuctionService.IntegrationTests.Fixtures;
+using AuctionService.IntegrationTests.Utils;
+using Contracts;
+using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionService.IntegrationTests;
+
+public class AuctionBusTests : IClassFixture<CustomWebAppFactory>, IAsyncLifetime
+{
+    private readonly CustomWebAppFactory _factory;
+    private readonly HttpClient _httpClient;
+    private readonly ITestHarness _testHarness;
+
+    public AuctionBusTests(CustomWebAppFactory factory)
+    {
+        _factory = factory;
+        _httpClient = factory.CreateClient();
+        _testHarness = factory.Services.GetTestHarness();
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+
+    public Task DisposeAsync()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AuctionDbContext>();
+        DbHelper.ReinitializeDbForTests(db);
+        return Task.CompletedTask;
+    }
+
+
+
+    [Fact]
+    public async Task CreateAuction_WithValidObject_ShouldPublishAuctionCreated()
+    {
+        // Arrange
+        var auction = TestData.GetTestAuction();
+        _httpClient.SetFakeJwtBearerToken(AuthHelper.GetBearerForUser(TestConstants.AliceUser));
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync(TestConstants.Endpoint, auction);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        Assert.True(await _testHarness.Published.Any<AuctionCreated>());
+    }
+
+
+
+}
